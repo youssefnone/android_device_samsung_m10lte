78139e207e16c5ed5ba6ae315f6be9f810af1af0
youssefnone <113144168+youssefnone@users.noreply.github.com>
2023-02-02 18:29:18 +0100

    Update vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
 
+# MTP
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb
+
 # Fix for echo in calls
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.disable_two_mic=false
